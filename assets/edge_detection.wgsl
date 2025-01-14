397a766f482c11f3368fd911fd65f63916021a30
allenpkt <allenpocketgamer@gmail.com>
2025-01-14 11:01:17 +0800

    add simple example

--- assets/edge_detection.wgsl
+++ assets/edge_detection.wgsl
@@ -68,12 +68,6 @@ fn position_ndc_to_world(ndc_pos: vec3f) -> vec3f {
     return world_pos.xyz / world_pos.w;
 }
 
-/// Convert a view space direction to world space
-fn direction_view_to_world(view_dir: vec3<f32>) -> vec3<f32> {
-    let world_dir = view.world_from_view * vec4(view_dir, 0.0);
-    return world_dir.xyz;
-}
-
 fn calculate_view(world_position: vec3f) -> vec3f {
 #ifdef VIEW_PROJECTION_ORTHOGRAPHIC
         // Orthographic view vector
@@ -245,8 +239,6 @@ fn fragment(
     texture_size = vec2f(textureDimensions(screen_texture));
     texel_size = 1.0 / texture_size;
 
-    // let noise = perlinNoise2(in.uv * vec2f(1280.0, 720.0) / 16.0);
-    // let noise_y = perlinNoise2((in.uv + vec2f(0.5)) * vec2f(1280.0, 720.0) / 16.0);
     let uv = in.uv;
 
     let ndc = vec3f(uv_to_ndc(uv), 1.0);
@@ -259,7 +251,7 @@ fn fragment(
     var edge = 0.0;
 
 #ifdef ENABLE_DEPTH
-    let edge_depth = detect_edge_depth(uv, ed_uniform.depth_thickness, fresnel);
+    let edge_depth = detect_edge_depth(uv, ed_uniform.depth_thickness + 1.2 * noise, fresnel);
     edge = max(edge, edge_depth);
 #endif
 
@@ -277,4 +269,42 @@ fn fragment(
     color = mix(color, ed_uniform.edge_color.rgb, edge);
 
     return vec4f(color, 1.0);
+}
+
+// MIT License. © Stefan Gustavson, Munrocket
+//
+fn permute4(x: vec4f) -> vec4f { return ((x * 34. + 1.) * x) % vec4f(289.); }
+fn fade2(t: vec2f) -> vec2f { return t * t * t * (t * (t * 6. - 15.) + 10.); }
+
+fn perlinNoise2(P: vec2f) -> f32 {
+    var Pi: vec4f = floor(P.xyxy) + vec4f(0., 0., 1., 1.);
+    let Pf = fract(P.xyxy) - vec4f(0., 0., 1., 1.);
+    Pi = Pi % vec4f(289.); // To avoid truncation effects in permutation
+    let ix = Pi.xzxz;
+    let iy = Pi.yyww;
+    let fx = Pf.xzxz;
+    let fy = Pf.yyww;
+    let i = permute4(permute4(ix) + iy);
+    var gx: vec4f = 2. * fract(i * 0.0243902439) - 1.; // 1/41 = 0.024...
+    let gy = abs(gx) - 0.5;
+    let tx = floor(gx + 0.5);
+    gx = gx - tx;
+    var g00: vec2f = vec2f(gx.x, gy.x);
+    var g10: vec2f = vec2f(gx.y, gy.y);
+    var g01: vec2f = vec2f(gx.z, gy.z);
+    var g11: vec2f = vec2f(gx.w, gy.w);
+    let norm = 1.79284291400159 - 0.85373472095314 *
+        vec4f(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+    g00 = g00 * norm.x;
+    g01 = g01 * norm.y;
+    g10 = g10 * norm.z;
+    g11 = g11 * norm.w;
+    let n00 = dot(g00, vec2f(fx.x, fy.x));
+    let n10 = dot(g10, vec2f(fx.y, fy.y));
+    let n01 = dot(g01, vec2f(fx.z, fy.z));
+    let n11 = dot(g11, vec2f(fx.w, fy.w));
+    let fade_xy = fade2(Pf.xy);
+    let n_x = mix(vec2f(n00, n01), vec2f(n10, n11), vec2f(fade_xy.x));
+    let n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+    return 2.3 * n_xy;
 }
---
+++ examples/simple.rs
@@ -0,0 +1,135 @@
+//! This example comes from [3d_shapes](https://github.com/bevyengine/bevy/blob/main/examples/3d/3d_shapes.rs)
+
+use bevy::{
+    core_pipeline::{core_3d::graph::Node3d, smaa::Smaa},
+    prelude::*,
+};
+use bevy_edge_detection::{EdgeDetection, EdgeDetectionPlugin};
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(EdgeDetectionPlugin {
+            // If you wish to apply Smaa anti-aliasing after edge detection,
+            // please ensure that the rendering order of [`EdgeDetectionNode`] is set before [`SmaaNode`].
+            before: Node3d::Smaa,
+        })
+        .add_plugins(EguiPlugin)
+        .add_plugins(PanOrbitCameraPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, edge_detection_ui)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::from_length(8.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.6509, 0.6509, 0.6509),
+            unlit: true,
+            ..default()
+        })),
+        Transform::from_scale(Vec3::new(1.0, 0.5, 1.0)),
+    ));
+
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            intensity: 10_000_000.,
+            range: 100.0,
+            shadow_depth_bias: 0.2,
+            ..default()
+        },
+        Transform::from_xyz(8.0, 16.0, 8.0),
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 7., 14.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        Camera {
+            clear_color: Color::WHITE.into(),
+            ..default()
+        },
+        // [`EdgeDetectionNode`] supports `Msaa``, and you can enable it at any time, for example:
+        // Msaa::default(),
+        Msaa::Off,
+        EdgeDetection::default(),
+        Smaa::default(),
+        // to control camera
+        PanOrbitCamera::default(),
+    ));
+}
+
+fn edge_detection_ui(mut ctx: EguiContexts, mut edge_detection: Single<&mut EdgeDetection>) {
+    egui::Window::new("Edge Detection Settings").show(ctx.ctx_mut(), |ui| {
+        ui.vertical(|ui| {
+            ui.horizontal(|ui| {
+                ui.add(egui::Checkbox::new(
+                    &mut edge_detection.enable_depth,
+                    "enable_depth",
+                ));
+                ui.add(
+                    egui::Slider::new(&mut edge_detection.depth_threshold, 0.0..=8.0)
+                        .text("depth_threshold"),
+                );
+            });
+
+            ui.horizontal(|ui| {
+                ui.add(egui::Checkbox::new(
+                    &mut edge_detection.enable_normal,
+                    "enable_normal",
+                ));
+                ui.add(
+                    egui::Slider::new(&mut edge_detection.normal_threshold, 0.0..=8.0)
+                        .text("normal_threshold"),
+                );
+            });
+
+            ui.horizontal(|ui| {
+                ui.add(egui::Checkbox::new(
+                    &mut edge_detection.enable_color,
+                    "enable_color",
+                ));
+                ui.add(
+                    egui::Slider::new(&mut edge_detection.color_threshold, 0.0..=8.0)
+                        .text("color_threshold"),
+                );
+            });
+
+            ui.add(
+                egui::Slider::new(&mut edge_detection.depth_thickness, 0.0..=8.0)
+                    .text("depth_thickness"),
+            );
+            ui.add(
+                egui::Slider::new(&mut edge_detection.normal_thickness, 0.0..=8.0)
+                    .text("normal_thickness"),
+            );
+            ui.add(
+                egui::Slider::new(&mut edge_detection.color_thickness, 0.0..=8.0)
+                    .text("color_thickness"),
+            );
+
+            ui.add(
+                egui::Slider::new(&mut edge_detection.steep_angle_threshold, 0.0..=1.0)
+                    .text("steep_angle_threshold"),
+            );
+            ui.add(
+                egui::Slider::new(&mut edge_detection.steep_angle_multiplier, 0.0..=1.0)
+                    .text("steep_angle_multiplier"),
+            );
+
+            let mut color = edge_detection.edge_color.to_srgba().to_f32_array_no_alpha();
+            ui.horizontal(|ui| {
+                egui::color_picker::color_edit_button_rgb(ui, &mut color);
+                ui.label("edge_color");
+            });
+            edge_detection.edge_color = Color::srgb_from_array(color);
+        });
+    });
+}
